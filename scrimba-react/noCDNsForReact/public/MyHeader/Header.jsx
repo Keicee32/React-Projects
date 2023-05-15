1d54977c8500d5aed648703862a3486acfb33ff0
Keicee <kelss451@googlemail.com>
2023-05-15 01:10:59 +0100

    used components

---
+++ scrimba-react/noCDNsForReact/public/MyHeader/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./header.css"
+
+function MyHeader() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Home</li>   
+                        <li>About React</li>   
+                        <li>Contact</li>   
+                    </ul>    
+                </nav>    
+            </header>        
+        
+        </>
+    )
+}
+
+export default MyHeader
