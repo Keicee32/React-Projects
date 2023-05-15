7c1df09541dde5923890189bc1f15200983be83a
Keicee <kelss451@googlemail.com>
2023-05-15 01:15:29 +0100

    added Footer

---
+++ scrimba-react/noCDNsForReact/public/MyFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MyFooter() {
+  return (
+    <>
+        <footer>
+            <small>© 2021 Kenz development. All rights reserved.</small>
+        </footer>
+    </>
+  )
+}
+
+export default MyFooter
